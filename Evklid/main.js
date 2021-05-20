4a6883a5e2e502ab38f042c7b99b02263ee8edae
Marat <plievmar@gmail.com>
2021-05-20 13:45:26 +0300

    Добавил свой учебный проект
    Разрешение 1920/1024/720/320
    Внести корректировки в адаптивных версиях

---
+++ Evklid/main.js
@@ -0,0 +1,45 @@
+var navbar = document.querySelector(".navbar")
+var ham = document.querySelector(".ham")
+
+// toggles hamburger menu in and out when clicking on the hamburger
+function toggleHamburger(){
+  navbar.classList.toggle("showNav")
+  ham.classList.toggle("showClose")
+}
+
+ham.addEventListener("click", toggleHamburger)
+
+// toggle when clicking on links
+
+// METHOD 1
+var menuLinks = document.querySelectorAll(".menuLink")
+menuLinks.forEach( 
+  function(menuLink) { 
+    menuLink.addEventListener("click", toggleHamburger) 
+  }
+)
+
+    var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    loopedSlides: 3,
+    autoplay: {
+    delay: 3000,
+    },
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        hide: true,
+    },
+    })
