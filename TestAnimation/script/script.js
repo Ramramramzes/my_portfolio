ea11d74c8592fe480b1d3edea67fdaf2d566ff6e
Marat <plievmar@gmail.com>
2021-05-22 19:01:43 +0300

    Делал тестовую анимацию с помощью gsap
    Не закрывается бургер, доработать

---
+++ TestAnimation/script/script.js
@@ -0,0 +1,25 @@
+gsap.timeline()
+.from(".hero__title, .hero__btn", {opacity:0, y:50, duration:1})
+.fromTo(".hero__descr", {opacity: 0}, {opacity: 1, duration: 1})
+.fromTo(".photos-wrap__first", {opacity:0, scale: 0.8}, {opacity: 1, duration: 0.3, scale: 1})
+.fromTo(".photos-wrap__second", {opacity:0, scale: 0.8}, {opacity: 1, duration: 0.3, scale: 1})
+.fromTo(".photos-wrap__third", {opacity:0, scale: 0.8}, {opacity: 1, duration: 0.3, scale: 1})
+
+var menu = document.querySelector(".menu");
+var burger = document.querySelector(".burger");
+
+var tl = gsap.timeline({paused: true});
+
+
+  tl.fromTo(".menu", {display:'none',opacity: 0, y: 300}, {display:'block', opacity: 1, duration:.4, y: 0})
+            .fromTo(".menu__top", {opacity: 0,y: -15}, {opacity: 1, duration: .5, y: 0})
+            .fromTo(".menu__container", {opacity: 0,y: 15}, {opacity: 1, duration: .3, y: 0})
+            .fromTo(".nav__list", {opacity: 0,y: 100}, {opacity: 1, duration: .8, y: 0})
+            .fromTo(".social, .sub-menu", {opacity: 0,y: 100}, {opacity: 1, duration: .8, y: 0})
+
+
+burger.onclick = function(){
+    tl.play();
+  }
+
+
