a0653a66789549ac5a6dae62d22f232a5a5c3a2e
Marat <plievmar@gmail.com>
2021-05-24 13:06:46 +0300

    Переделал анимацию
    Переделал анимацию главной страницы
    Исправил бургер, теперь закрывается и открывается)

--- TestAnimation/script/script.js
+++ TestAnimation/script/script.js
@@ -1,10 +1,9 @@
 gsap.timeline()
 .from(".hero__title, .hero__btn", {opacity:0, y:50, duration:1})
-.fromTo(".hero__descr", {opacity: 0}, {opacity: 1, duration: 2}, .8)
-.fromTo(".photos-wrap__first", {opacity:0, scale: 0.8}, {opacity: 1, duration: 0.5, scale: 1},.8)
-.fromTo(".photos-wrap__second", {opacity:0, scale: 0.8}, {opacity: 1, duration: 0.3, scale: 1},1.2)
-.fromTo(".photos-wrap__third", {opacity:0, scale: 0.8}, {opacity: 1, duration: 0.3, scale: 1},1.4)
-
+.fromTo(".hero__descr", {opacity: 0}, {opacity: 1, duration: 1.5},1.1)
+.fromTo(".photos-wrap__first", {opacity:0, scale: 0.8}, {opacity: 1, duration: 0.5, scale: 1},1.1)
+.fromTo(".photos-wrap__second", {opacity:0, scale: 0.8}, {opacity: 1, duration: 0.3, scale: 1},1.5)
+.fromTo(".photos-wrap__third", {opacity:0, scale: 0.8}, {opacity: 1, duration: 0.3, scale: 1},1.8)
 
 var menu = document.querySelector(".menu");
 var burger = document.querySelector(".burger");
@@ -13,9 +12,9 @@ var close = document.querySelector(".close");
 var tl = gsap.timeline({paused: true});
 
 
-  tl.fromTo(".menu", {display:'none',opacity: 0, y: 300}, {display:'block', opacity: 1, duration:.4, y: 0})
-            .fromTo(".menu__top", {opacity: 0,y: -15}, {opacity: 1, duration: .1, y: 0})
-            .fromTo(".menu__container", {opacity: 0,y: 15}, {opacity: 1, duration: .1, y: 0})
+  tl.fromTo(".menu", {display:'none',opacity: 0, y: 300}, {display:'block', opacity: 1, duration:.3, y: 0})
+            .fromTo(".menu__top", {opacity: 0,y: -15}, {opacity: 1, duration: .3, y: 0})
+            .fromTo(".menu__container", {opacity: 0,y: 15}, {opacity: 1, duration: .2, y: 0})
             .fromTo(".nav__list", {opacity: 0,y: 100}, {opacity: 1, duration: .4, y: 0})
             .fromTo(".social, .sub-menu", {opacity: 0,y: 100}, {opacity: 1, duration: .4, y: 0})
 
@@ -34,5 +33,3 @@ close.onclick = function(){
     tl.play();
   }
 }
-
-
