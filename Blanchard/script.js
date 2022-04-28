84cc844894d0f06e2bab52ce58486f7c816efd4b
Marat <plievmar@gmail.com>
2022-04-28 22:16:30 +0300

    Блок gallery для 1920

--- Blanchard/script.js
+++ Blanchard/script.js
@@ -12,12 +12,15 @@ document.getElementById('burger').addEventListener('click',function(){
         let custom3 = document.getElementById('3custom');
         let check3 = document.getElementById('3chbx');
         let lblchbx3 = document.getElementById('label-3chbx');
+        
         custom1.addEventListener('click',function(){
             if(check1.checked !== true){
                 custom1.classList.add('activ-border')
+                lblchbx1.classList.add('activ-color')
                 check1.checked = true;
             }else{
                 custom1.classList.remove('activ-border')
+                lblchbx1.classList.remove('activ-color')
                 check1.checked = false;
             }
         })
@@ -26,18 +29,21 @@ document.getElementById('burger').addEventListener('click',function(){
             if(check1.checked !== true){
                 check1.checked = false;
                 custom1.classList.add('activ-border')
-
+                lblchbx1.classList.add('activ-color')
             }else{
                 custom1.classList.remove('activ-border')
+                lblchbx1.classList.remove('activ-color')
                 check1.checked = true;
             }
         })
         custom2.addEventListener('click',function(){
             if(check2.checked !== true){
                 custom2.classList.add('activ-border')
+                lblchbx2.classList.add('activ-color')
                 check2.checked = true;
             }else{
                 custom2.classList.remove('activ-border')
+                lblchbx2.classList.remove('activ-color')
                 check2.checked = false;
             }
         })
@@ -45,17 +51,21 @@ document.getElementById('burger').addEventListener('click',function(){
             if(check2.checked !== true){
                 check2.checked = false;
                 custom2.classList.add('activ-border')
+                lblchbx2.classList.add('activ-color')
             }else{
                 custom2.classList.remove('activ-border')
+                lblchbx2.classList.remove('activ-color')
                 check2.checked = true;
             }
         })
         custom3.addEventListener('click',function(){
             if(check3.checked !== true){
                 custom3.classList.add('activ-border')
+                lblchbx3.classList.add('activ-color')
                 check3.checked = true;
             }else{
                 custom3.classList.remove('activ-border')
+                lblchbx3.classList.remove('activ-color')
                 check3.checked = false;
             }
         })
@@ -63,9 +73,12 @@ document.getElementById('burger').addEventListener('click',function(){
             if(check3.checked !== true){
                 check3.checked = false;
                 custom3.classList.add('activ-border')
+                lblchbx3.classList.add('activ-color')
             }else{
                 custom3.classList.remove('activ-border')
+                lblchbx3.classList.remove('activ-color')
                 check3.checked = true;
             }
         })
 //!--------------------------------------------------/
+//! Слайдер------------------------------------------/
